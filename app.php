9261919ef40432569ebdce216dc74d121d836461
Cristian Hampus <contact@cristianhampus.se>
2011-05-17 17:36:58 +0200

    Initial commit

---
+++ app.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once(__DIR__.'/silex/silex.phar');
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Extension\MonologExtension(), array(
+    'monolog.name' => 'app',
+    'monolog.logfile' => __DIR__.'/log/development.log',
+    'monolog.class_path' => __DIR__.'/vendor/monolog/src'
+));
+
+$app->register(new Silex\Extension\TwigExtension(), array(
+    'twig.path' => __DIR__.'/views',
+    'twig.options' => array(
+        'cache' => __DIR__.'/cache',
+        'debug' => true
+    ),
+    'twig.class_path' => __DIR__.'/vendor/twig/lib'
+));
+
+return $app;
